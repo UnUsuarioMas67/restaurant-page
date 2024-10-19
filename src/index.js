05e31d274b74c228984932371c787e7edebb0e14
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-10-19 01:19:24 -0400

    Implement nav buttons functionality
    This will allow to change the content that is being rendered by clicking the nav buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import "./style.css";
 import home from "./home.js";
+import menu from "./menu.js";
 
 const content = document.querySelector("#content");
+const navButtons = document.querySelectorAll("nav button");
 
-content.textContent = "";
-content.appendChild(home.getContent());
+navButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    handleNavBtnClick(btn)
+  })
+})
+
+const handleNavBtnClick = function (btn) {
+  navButtons.forEach((b) => {
+    if (b === btn) {
+      b.classList.add("selected")
+    } else {
+      b.classList.remove("selected")
+    }
+  })
+
+  switch (btn.id) {
+    case "home-btn":
+      renderContent(home.getContent())
+      break;
+    case "menu-btn":
+      renderContent(menu.getContent())
+      break;
+  }
+}
+
+const renderContent = function (newContent) {
+  content.textContent = "";
+  content.appendChild(newContent)
+}
+
+renderContent(home.getContent());
