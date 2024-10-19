1711c28e034bc95bf3bf5b9423f2e86475b07cc6
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-10-19 17:49:28 -0400

    Enable accessing About page from nav bar

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
 import "./style.css";
 import home from "./home.js";
 import menu from "./menu.js";
+import about from "./about.js";
 
 const content = document.querySelector("#content");
 const navButtons = document.querySelectorAll("nav button");
 
 navButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    handleNavBtnClick(btn)
-  })
-})
+    handleNavBtnClick(btn);
+  });
+});
 
 const handleNavBtnClick = function (btn) {
   navButtons.forEach((b) => {
     if (b === btn) {
-      b.classList.add("selected")
+      b.classList.add("selected");
     } else {
-      b.classList.remove("selected")
+      b.classList.remove("selected");
     }
-  })
+  });
 
   switch (btn.id) {
     case "home-btn":
-      renderContent(home.getContent())
+      renderContent(home.getContent());
       break;
     case "menu-btn":
-      renderContent(menu.getContent())
+      renderContent(menu.getContent());
+      break;
+    case "about-btn":
+      renderContent(about.getContent());
       break;
   }
-}
+};
 
 const renderContent = function (newContent) {
   content.textContent = "";
-  content.appendChild(newContent)
-}
+  content.appendChild(newContent);
+};
 
 renderContent(home.getContent());
